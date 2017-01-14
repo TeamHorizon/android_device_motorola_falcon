e884a7197b4793cca29fc0ca1decbe9521d8ff63
Aayushya Aankul <aayushyaaankul@gmail.com>
2017-01-14 22:48:19 +0530

    Add Falcon maintainer

--- xenonhd.mk
+++ xenonhd.mk
@@ -6,4 +6,6 @@ $(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := xenonhd_falcon
 
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=Aayushya
+
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
